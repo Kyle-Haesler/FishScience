9fa57d72f50cc052136866d0d931203a4fdd93fc
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-05 14:16:39 -0500

    Currently working on header

--- front-end/src/sections/Header.js
+++ front-end/src/sections/Header.js
@@ -1,5 +1,23 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="p-3 bg-dark text-white">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+          >
+            <img
+              src="https://images.freeimages.com/images/large-previews/865/tropical-fish-1176775.jpg"
+              width="20"
+              height="20"
+              alt="logo"
+            />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
