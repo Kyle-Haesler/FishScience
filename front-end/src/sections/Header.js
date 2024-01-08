821c921919c9fd3d06f5a40fd0bacdfb385cca85
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-08 12:51:42 -0500

    Working on building out a shell for the header

--- front-end/src/sections/Header.js
+++ front-end/src/sections/Header.js
@@ -3,17 +3,47 @@ const Header = () => {
     <header className="p-3 bg-dark text-white">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a
-            href="/"
-            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-          >
-            <img
-              src="https://images.freeimages.com/images/large-previews/865/tropical-fish-1176775.jpg"
-              width="20"
-              height="20"
-              alt="logo"
-            />
-          </a>
+          <div className="bg-white rounded-circle p-3">
+            <h1 className="text-black">FishScience</h1>
+          </div>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="/" className="nav-link px-2 text-secondary">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link px-2 text-white">
+                New
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link px-2 text-white">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link px-2 text-white">
+                Help
+              </a>
+            </li>
+          </ul>
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input
+              type="search"
+              className="form-control form-control-dark"
+              placeholder="Search..."
+              aria-label="Search"
+            ></input>
+          </form>
+          <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">
+              Login
+            </button>
+            <button type="button" className="btn btn-warning">
+              Sign-up
+            </button>
+          </div>
         </div>
       </div>
     </header>
