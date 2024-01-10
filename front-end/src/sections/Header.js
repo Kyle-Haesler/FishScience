e4e1c8737d61274b0843bda9933b6a722ae77cf9
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-10 13:44:41 -0500

    Header almost complete, have to make responsive and add link to logo

--- front-end/src/sections/Header.js
+++ front-end/src/sections/Header.js
@@ -3,45 +3,21 @@ import Logo from "../components/Logo";
 const Header = () => {
   return (
     <header className="p-3 bg-dark text-white">
-      <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div className="container-fluid">
+        <div className="d-flex align-items-center justify-content-between">
           <Logo />
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li>
-              <a href="/" className="nav-link px-2 text-secondary">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/" className="nav-link px-2 text-white">
-                New
-              </a>
-            </li>
-            <li>
-              <a href="/" className="nav-link px-2 text-white">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/" className="nav-link px-2 text-white">
-                Help
-              </a>
-            </li>
-          </ul>
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input
-              type="search"
-              className="form-control form-control-dark"
-              placeholder="Search..."
-              aria-label="Search"
-            ></input>
-          </form>
-          <div className="text-end">
-            <button type="button" className="btn btn-outline-light me-2">
-              Login
-            </button>
-            <button type="button" className="btn btn-warning">
-              Sign-up
+          <div className="d-flex align-items-center">
+            <div className="border border-white p-1 rounded">Signed in as:</div>
+            <div className="bg-white text-black border border-dark p-1 rounded">
+              Mr Potato Head
+            </div>
+
+            <button
+              type="button"
+              className="btn btn-warning"
+              style={{ "margin-left": "20px" }}
+            >
+              Logout
             </button>
           </div>
         </div>
