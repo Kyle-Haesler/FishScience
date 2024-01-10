369e916a0c5fd92fcde07b12b6640868a08a7df6
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-10 13:59:58 -0500

    Header near complete, need to adjust margin and add link to logo

--- front-end/src/components/Logo.js
+++ front-end/src/components/Logo.js
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="bg-white rounded-circle p-3">
-      <h1 className="text-black">FishScience</h1>
+      <h1 className="text-black text-center">FishScience</h1>
     </div>
   );
 };
--- front-end/src/sections/Header.js
+++ front-end/src/sections/Header.js
@@ -4,19 +4,15 @@ const Header = () => {
   return (
     <header className="p-3 bg-dark text-white">
       <div className="container-fluid">
-        <div className="d-flex align-items-center justify-content-between">
+        <div className="d-md-flex align-items-center justify-content-between">
           <Logo />
-          <div className="d-flex align-items-center">
+          <div className="d-flex justify-content-center align-items-center">
             <div className="border border-white p-1 rounded">Signed in as:</div>
             <div className="bg-white text-black border border-dark p-1 rounded">
               Mr Potato Head
             </div>
 
-            <button
-              type="button"
-              className="btn btn-warning"
-              style={{ "margin-left": "20px" }}
-            >
+            <button type="button" className="btn btn-warning ml-md-100">
               Logout
             </button>
           </div>
